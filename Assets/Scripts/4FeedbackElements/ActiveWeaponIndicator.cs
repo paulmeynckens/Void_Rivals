d3bad9abb1d02d8c750f8149427db4f7d8688444
prod\pmeynckens <63119095+paulmeynckens@users.noreply.github.com>
2021-12-01 12:23:43 +0100

    enhancement #33

--- Assets/Scripts/4FeedbackElements/ActiveWeaponIndicator.cs
+++ Assets/Scripts/4FeedbackElements/ActiveWeaponIndicator.cs
@@ -2,17 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using ShipsLogic.Guns;
+using UnityEngine.UI;
 
 namespace FeedbackElements
 {
     public class ActiveWeaponIndicator : MonoBehaviour
     {
         [SerializeField] ShipGun shipGun=null;
-        [SerializeField] GameObject activeAmmoIndicator = null;
+        Image image;
+
+        private void Awake()
+        {
+            image = GetComponent<Image>();
+        }
         // Update is called once per frame
         void FixedUpdate()
         {
-            activeAmmoIndicator.SetActive(shipGun.enabled);
+            if (shipGun.enabled)
+            {
+                image.color = Color.green;
+            }
+            else
+            {
+                image.color = Color.black;
+            }
+
         }
     }
 }
